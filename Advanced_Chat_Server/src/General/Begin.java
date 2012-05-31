a64f4c5cbf86eec99ea52a035396bd7a81f62138
teamprojectinfo <teamprojectinfo@e67156ed-0023-550c-18b5-fadc649641af>
2012-05-31 17:45:03 +0000

    ...

--- Advanced_Chat_Server/src/General/Begin.java
+++ Advanced_Chat_Server/src/General/Begin.java
@@ -42,8 +42,8 @@ public class Begin extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jTextField3 = new javax.swing.JTextField();
+        pswAdmin = new javax.swing.JPasswordField();
+        userAdmin = new javax.swing.JTextField();
         serverStateField = new javax.swing.JTextField();
         serverState = new javax.swing.JLabel();
 
@@ -59,22 +59,20 @@ public class Begin extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/General/Images/logo2.png"))); // NOI18N
 
-        jTextField1.setBackground(new java.awt.Color(254, 254, 254));
         jTextField1.setEditable(false);
         jTextField1.setFont(new java.awt.Font("SansSerif", 1, 15));
         jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         jTextField1.setText("Welcome Administrator!");
 
-        jTextField2.setBackground(new java.awt.Color(254, 254, 254));
         jTextField2.setEditable(false);
         jTextField2.setFont(new java.awt.Font("SansSerif", 1, 15));
         jTextField2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         jTextField2.setText("Login:");
 
         serverStateField.setEditable(false);
-        serverStateField.setFont(new java.awt.Font("SansSerif", 1, 15)); // NOI18N
+        serverStateField.setFont(new java.awt.Font("SansSerif", 1, 15));
 
-        serverState.setFont(new java.awt.Font("SansSerif", 1, 15)); // NOI18N
+        serverState.setFont(new java.awt.Font("SansSerif", 1, 15));
         serverState.setText("Stato del Server:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -93,8 +91,8 @@ public class Begin extends javax.swing.JFrame {
                                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField3)
-                                    .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE))))
+                                    .addComponent(userAdmin)
+                                    .addComponent(pswAdmin, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE))))
                         .addGap(155, 155, 155))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(170, 170, 170)
@@ -112,16 +110,16 @@ public class Begin extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(30, 30, 30)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 541, Short.MAX_VALUE))
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 547, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(126, 126, 126)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(26, 26, 26)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(userAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(28, 28, 28)
-                        .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(pswAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 240, Short.MAX_VALUE)
                         .addComponent(serverState, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -138,8 +136,8 @@ public class Begin extends javax.swing.JFrame {
     //start del server;
     private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
 
+        if(userAdmin.getText()!="" && pswAdmin.getText()!=""){
         try {
-
 
             Database.connectDb();
 
@@ -163,6 +161,7 @@ public class Begin extends javax.swing.JFrame {
         serverStateField.setText("Running");
         
         this.setVisible(false);
+        }
     }//GEN-LAST:event_startButtonActionPerformed
 
     /**
@@ -185,13 +184,13 @@ public class Begin extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JPasswordField pswAdmin;
     private javax.swing.JLabel serverState;
     private javax.swing.JTextField serverStateField;
     private javax.swing.JButton startButton;
+    private javax.swing.JTextField userAdmin;
     // End of variables declaration//GEN-END:variables
 
 }
--- Advanced_Chat_Server/src/General/generalView.java
+++ Advanced_Chat_Server/src/General/generalView.java
@@ -10,10 +10,14 @@
  */
 package General;
 
+import Network.serverExecutor;
+import XML.XML;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Vector;
 import java.util.concurrent.Semaphore;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -36,6 +40,12 @@ public class generalView extends javax.swing.JFrame implements Runnable{
     }
 
     
+    public void setExecutor(serverExecutor se){
+        
+        this.se = se;
+        
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -50,7 +60,10 @@ public class generalView extends javax.swing.JFrame implements Runnable{
         eventList = new javax.swing.JTextArea();
         jScrollPane1 = new javax.swing.JScrollPane();
         loggedUsersList = new javax.swing.JList();
-        dumpButton = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        dumpMenuItem = new javax.swing.JMenu();
+        signUser = new javax.swing.JMenuItem();
+        dumpDB = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -75,7 +88,7 @@ public class generalView extends javax.swing.JFrame implements Runnable{
         jPanel13.setLayout(jPanel13Layout);
         jPanel13Layout.setHorizontalGroup(
             jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 954, Short.MAX_VALUE)
+            .addGap(0, 970, Short.MAX_VALUE)
         );
         jPanel13Layout.setVerticalGroup(
             jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -90,7 +103,7 @@ public class generalView extends javax.swing.JFrame implements Runnable{
         DecorationFunctionPanel1.setLayout(DecorationFunctionPanel1Layout);
         DecorationFunctionPanel1Layout.setHorizontalGroup(
             DecorationFunctionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 954, Short.MAX_VALUE)
+            .addGap(0, 970, Short.MAX_VALUE)
         );
         DecorationFunctionPanel1Layout.setVerticalGroup(
             DecorationFunctionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -106,12 +119,27 @@ public class generalView extends javax.swing.JFrame implements Runnable{
 
         jScrollPane1.setViewportView(loggedUsersList);
 
-        dumpButton.setText("Dump");
-        dumpButton.addActionListener(new java.awt.event.ActionListener() {
+        dumpMenuItem.setText("Operazioni");
+
+        signUser.setText("Aggiungi un utente alla BlackList");
+        signUser.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                dumpButtonActionPerformed(evt);
+                signUserActionPerformed(evt);
             }
         });
+        dumpMenuItem.add(signUser);
+
+        dumpDB.setText("Fai un backup del Database");
+        dumpDB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dumpDBActionPerformed(evt);
+            }
+        });
+        dumpMenuItem.add(dumpDB);
+
+        jMenuBar1.add(dumpMenuItem);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -124,17 +152,19 @@ public class generalView extends javax.swing.JFrame implements Runnable{
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 786, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(dumpButton, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 509, Short.MAX_VALUE)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(290, 290, 290)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(logoutButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGap(241, 241, 241)
+                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
+                                        .addComponent(logoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 808, Short.MAX_VALUE)))
                     .addComponent(jPanel13, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(DecorationFunctionPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
@@ -144,20 +174,18 @@ public class generalView extends javax.swing.JFrame implements Runnable{
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(DecorationFunctionPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(36, 36, 36)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(logoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(dumpButton)))
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
+                                .addComponent(logoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel2)))
@@ -175,11 +203,26 @@ private void logoutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-
         starter.setVisible(true);
 }//GEN-LAST:event_logoutButtonActionPerformed
 
-    private void dumpButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dumpButtonActionPerformed
+    private void dumpDBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dumpDBActionPerformed
         
+        XML xml = new XML();
+        xml.dumpDB();
         
+    }//GEN-LAST:event_dumpDBActionPerformed
+
+    private void signUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signUserActionPerformed
         
-    }//GEN-LAST:event_dumpButtonActionPerformed
+        if(!loggedUsersList.isSelectionEmpty()){
+            try{
+                int idtoSign = this.se.selectId(loggedUsersList.getSelectedValue().toString());
+                this.se.addToBlackList(idtoSign);
+            }
+            catch(SQLException ex){
+                
+                JOptionPane.showMessageDialog(null,"Problema riscontrato:\n"+ex.getMessage() , "ACES", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_signUserActionPerformed
 
     public void addLoggedUsers(String username) throws InterruptedException{
         
@@ -239,6 +282,7 @@ private void logoutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-
         
     }
     
+    private serverExecutor se;
     private Semaphore sem;
     private ArrayList<String> events;
     private Vector loggedUsers;
@@ -246,15 +290,18 @@ private void logoutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-
     private Begin starter;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel DecorationFunctionPanel1;
-    private javax.swing.JButton dumpButton;
+    private javax.swing.JMenuItem dumpDB;
+    private javax.swing.JMenu dumpMenuItem;
     private javax.swing.JTextArea eventList;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel13;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList loggedUsersList;
     private javax.swing.JButton logoutButton;
+    private javax.swing.JMenuItem signUser;
     // End of variables declaration//GEN-END:variables
 }
