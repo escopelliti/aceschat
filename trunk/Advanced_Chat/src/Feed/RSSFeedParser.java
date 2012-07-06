41c73868bca0a103f3f3c526ce77427e49a1c8b5
teamprojectinfo <teamprojectinfo@e67156ed-0023-550c-18b5-fadc649641af>
2012-07-06 15:59:21 +0000

    RSSFeedParser PRONTO - CONTROLLALO

--- trunk/Advanced_Chat/src/Feed/RSSFeedParser.java
+++ trunk/Advanced_Chat/src/Feed/RSSFeedParser.java
@@ -1,95 +1,94 @@
-
 package Feed;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.swing.JOptionPane;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 
+/*Classe che fa da Parser per i FeedRSS*/
 public class RSSFeedParser {
 
-	public RSSFeedParser(String feedUrl) {
+    //Nel costruttore definiamo l'URL da cui attingeremo per i Feed;
+    public RSSFeedParser(String feedUrl) {
+         
+        url = null;
+        try{
+            this.url = new URL(feedUrl);
+        }catch (MalformedURLException e){
+            JOptionPane.showMessageDialog(null,"Problemi tecnici. Ci scusiamo per l'inconveniente." , "ACES", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    /*"Parsa" i Feed eseguenza le lettura tramite XML tornando un oggetto Feed*/
+    @SuppressWarnings("null")
+    public Feed readFeed() {
             
-		try {
-			this.url = new URL(feedUrl);
-                        
-		} catch (MalformedURLException e) {
-                    
-			throw new RuntimeException(e);
-		}
-	}
+        Feed feed = null;
+	try{
+            boolean isFeedHeader = true;
+	    String title = "";
+            //Creiamo un XMLInputFactory
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            // Imposta un eventReader
+            InputStream in = read();
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+            // Legge il documento XML grazie all'eventReader
+            while (eventReader.hasNext()){
 
-	@SuppressWarnings("null")
-	public Feed readFeed() {
-            
-		Feed feed = null;
-		try {
-
-			boolean isFeedHeader = true;
-			String title = "";
-			// First create a new XMLInputFactory
-			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-			// Setup a new eventReader
-			InputStream in = read();
-			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-			// Read the XML document
-			while (eventReader.hasNext()) {
-
-				XMLEvent event = eventReader.nextEvent();
-
-				if (event.isStartElement()) {
-					if (event.asStartElement().getName().getLocalPart() == (ITEM)) {
+                XMLEvent event = eventReader.nextEvent();
+		if (event.isStartElement()) {
+                    if (event.asStartElement().getName().getLocalPart() == (ITEM)){
                                             
-						if (isFeedHeader) {
-							isFeedHeader = false;
-							feed = new Feed(title);
-						}
-						event = eventReader.nextEvent();
-						continue;
-					}
-
-					if (event.asStartElement().getName().getLocalPart() == (TITLE)) {
-                                            
-						event = eventReader.nextEvent();
-						title = event.asCharacters().getData();
-						continue;
-					}
-                                        
-				} else if (event.isEndElement()) {
-                                    
-					if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
-                                            
-						FeedMessage message = new FeedMessage();
-						message.setTitle(title);
-						feed.getMessages().add(message);
-						event = eventReader.nextEvent();
-						continue;
-					}
-				}
+                        if (isFeedHeader){
+                            isFeedHeader = false;
+                            feed = new Feed(title);
 			}
-		} catch (XMLStreamException e) {
-			throw new RuntimeException(e);
-		}
+			event = eventReader.nextEvent();
+			continue;
+                    }
+
+                    if (event.asStartElement().getName().getLocalPart() == (TITLE)){
+                                            
+                        event = eventReader.nextEvent();
+			title = event.asCharacters().getData();
+			continue;
+                    }
+                                        
+		}else if (event.isEndElement()){
+                                    
+                    if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
+                                           
+                        FeedMessage message = new FeedMessage();
+			message.setTitle(title);
+			feed.getMessages().add(message);
+			event = eventReader.nextEvent();
+        		continue;
+                    }
+            	}
+            }
+        }catch (XMLStreamException e) {
+            JOptionPane.showMessageDialog(null,"Problemi tecnici. Ci scusiamo per l'inconveniente." , "ACES", JOptionPane.ERROR_MESSAGE);
+	}
                 
-		return feed;
+        return feed;
+    }
 
-	}
-
-	private InputStream read() {
-		try {
-                    
-			return url.openStream();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-        
-        
-        private static final String TITLE = "title";
-	private static final String ITEM = "item";
-        private final URL url;
+    /*Otteniamo un oggetto per poter leggere dallo stream di connessione all'URL selezionato*/
+    private InputStream read(){
+        try{           
+            return url.openStream();
+	}catch (IOException e){
+            JOptionPane.showMessageDialog(null,"Problemi tecnici. Ci scusiamo per l'inconveniente." , "ACES", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+    }
+             
+    private static final String TITLE = "title";
+    private static final String ITEM = "item";
+    private URL url;
 }
