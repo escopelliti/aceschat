ddfdc6fc2177c19913726042dff6e72361dbef74
teamprojectinfo <teamprojectinfo@e67156ed-0023-550c-18b5-fadc649641af>
2012-07-06 08:45:20 +0000

    REQUESTS PRONTO - CONTROLLALO

--- Advanced_Chat/src/Networking/Requests.java
+++ Advanced_Chat/src/Networking/Requests.java
@@ -15,8 +15,11 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Vector;
 import javax.swing.JOptionPane;
 
+/*Con questa classe ci occupiamo delle richieste che il client inoltra al server.
+ * Nel costruttore creiamo un oggetto della classe Socket che ci permette tramite lo stream di input
+ * e output di comunicare col Server remoto.
+ */
 public class Requests {
-
 
     public Requests(String ip,String port) throws UnknownHostException, IOException{
 
@@ -24,7 +27,6 @@ public class Requests {
         this.port = port;
 
         Integer n_port = new Integer(port);
-
         socket = new Socket(this.ip,n_port);
         out = new ObjectOutputStream(socket.getOutputStream());
         in = new ObjectInputStream(socket.getInputStream());
@@ -33,31 +35,10 @@ public class Requests {
     public ObjectInputStream getIn() {
         return in;
     }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public ObjectOutputStream getOut() {
-        return out;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getPort() {
-        return port;
-    }
     
     public void getMess(String clientUsername) throws IOException{
         
-        this.out.writeObject(new Packet(9,clientUsername));
-        
-    }
-
-    public String getIp() {
-        return ip;
+        this.out.writeObject(new Packet(9,clientUsername));        
     }
    
     public void getFriends(int idUser) throws IOException{
@@ -67,7 +48,9 @@ public class Requests {
         packet = new Packet(6, idUser);
         this.out.writeObject(packet);
     }   
-    
+    /*Invio di un messaggio ad un altro client;è codificata la multichat ovvero il messaggio
+     * può avere più destinatari
+     */
     public void send(String message,Vector receiver,String sender) throws IOException{
 
         Packet packet;
@@ -80,7 +63,9 @@ public class Requests {
         packet = new Packet(8,toSend);
         this.out.writeObject(packet);
     }
-
+    /*Meccanismo che implementa l'autoregolazione della community. Un utente invia una segnalazione di abuso
+     * di modo da segnalare al server che agirà di conseguenza.
+     */
     public void sendAbuse(int idSender,String offense,String offender) throws IOException{
         
         Vector toSend;
@@ -91,10 +76,11 @@ public class Requests {
         toSend.add(1,offender);
         toSend.add(2, offense);
         packet = new Packet(5, toSend);
-        this.out.writeObject(packet);
-        
+        this.out.writeObject(packet);        
     }
-
+    /*Richiesta di registrazione di un utente; si attende la risposta del server, che in caso
+     * di ammissione dell'utente alla community,rende visibile la Home dell'applicazione.
+     */
     public boolean registerMe(User user) throws IOException, ClassNotFoundException{
 
         Packet toSend;
@@ -121,7 +107,7 @@ public class Requests {
 
 
     }
-    
+    /*Invia un'immagine scelta dall'utente per il suo profilo ACES.*/
     public void sendImage(File image,int idUser) throws IOException{
         
         Packet packet;
@@ -135,7 +121,7 @@ public class Requests {
         this.out.writeObject(packet);
   
     }
-    
+    /*Richiesta di invio di un invito ad usare l'applicazione all'indirizzo di posta selezionato*/
     public void sendMail(String sender,String mailToInvite) throws IOException{
 
         Packet packet;
@@ -148,7 +134,9 @@ public class Requests {
         packet = new Packet(3,vect);
         this.out.writeObject(packet);
     }
-
+    /*Invio Richiesta di login all'applicazione, in caso di avvenuta autenticazione, carica la home
+     * la Home dell'applicazione e le strutture dati necessarie per il corretto funzionamento.
+     */
     public boolean logMe(String username,String password) throws IOException, ClassNotFoundException, NoSuchAlgorithmException{
 
         Packet toSend;
@@ -176,6 +164,7 @@ public class Requests {
         }
     }
 
+    /*Inviamo un nuovo oggetto della classe User che comprende tutte le modifiche al profilo personale dell'utente*/
     public void modifyPersonalInfo(User user) throws IOException{
 
         Packet toSend;
@@ -183,17 +172,19 @@ public class Requests {
         toSend = new Packet(7, user);
 
         this.out.writeObject(toSend);
-
     }
-
+    /*Inviamo una richiesta al server di invio delle informazioni riguardanti l'utente
+     * identificato tramite l'username.
+     */
     public void searchUser(String username) throws IOException{
         
         Packet toSend;
         toSend = new Packet(13,username);
-        this.out.writeObject(toSend);
-             
+        this.out.writeObject(toSend);             
     }
-
+    /*L'utente tramite questo metodo invia una richiesta al server al fine di aggiungere un amico alla propria
+     * lista d'amici.
+     */
     public void addFriend(String friend,int id) throws IOException{
 
         Packet toSend;
@@ -206,9 +197,9 @@ public class Requests {
         toSend = new Packet(1,v);
 
         this.out.writeObject(toSend);
-
     }
 
+    /*Inviamo un file a più destinatari o ad un singolo destinario*/
     public void send(File file,Vector receiver,String sender) throws IOException{
 
         Packet packet;
@@ -222,7 +213,7 @@ public class Requests {
         this.out.writeObject(packet);
         this.out.flush();
     }
-    
+    /*Richiesta di logout dall'applicazione.*/
     public void logout(int id) throws IOException{
 
         Packet packet;   
@@ -239,22 +230,19 @@ public class Requests {
         this.socket.close();
     }
     
+    /*Richiede le statistiche riguardanti l'utente fin dalla registrazione del'utente.*/
     public void getStatistics(int IdUser) throws IOException{
         
         Packet packet;
         packet = new Packet(15,IdUser);
-        this.out.writeObject(packet);
-   
+        this.out.writeObject(packet);  
     }
-    
-    
+       
     private String ip;
     private String port;
     private Socket socket;
     private ObjectOutputStream out;
-    private ObjectInputStream in;
-    
-    
+    private ObjectInputStream in;    
 }
 
 
