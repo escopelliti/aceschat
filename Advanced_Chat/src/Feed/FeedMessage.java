125695b7f2b1f6209ba3c97db0a4539abee0fb23
teamprojectinfo <teamprojectinfo@e67156ed-0023-550c-18b5-fadc649641af>
2012-07-06 15:35:03 +0000

    FEEDMESSAGE.JAVA PRONTO - CONTROLLALO

--- Advanced_Chat/src/Feed/FeedMessage.java
+++ Advanced_Chat/src/Feed/FeedMessage.java
@@ -1,45 +1,23 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author enrico
- *
-*/
 package Feed;
 
-
+/*Classe che definisce un messaggio Feed*/
 public class FeedMessage {
 
-	String title;
-	String link;
+    public String getTitle() {
+        return title;
+    }
 
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-
-	public String getLink() {
-		return link;
-	}
-
-	public void setLink(String link) {
-		this.link = link;
-	}
-
+    public void setTitle(String title) {
+        this.title = title;
+    }  
 
 	@Override
-	public String toString() {
-		return "FeedMessage [title=" + title + ","+" link=" + link
-				+ "]";
-	}
-
+    public String toString(){
+	
+        return "FeedMessage [title=" + title + ","+" link=" + link + "]";				
+    }
+        
+    String title;
+    String link;
 }
 
