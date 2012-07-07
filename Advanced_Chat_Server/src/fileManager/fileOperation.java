7f8acd1aacdb1d8ff8ac25001395569e88609c5c
teamprojectinfo <teamprojectinfo@e67156ed-0023-550c-18b5-fadc649641af>
2012-07-07 09:24:51 +0000

    FILE OPERATION SERVER PRONTO - CONTROLLALO

--- Advanced_Chat_Server/src/fileManager/fileOperation.java
+++ Advanced_Chat_Server/src/fileManager/fileOperation.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package fileManager;
 
+
 import java.awt.Image;
-import java.util.Properties;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,46 +9,27 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-/**
- *
- * @author enrico  classe per gestire creare e cancellare le cartelle che servono al software
- */
+
+
 public class fileOperation {
 
-
-        public static boolean createDirectory(String dir){  //crea una cartella nel percorso specificato in dir
+    //Permette la creazione di files e directory;
+    public boolean createFiles(String dir){  //crea una cartella nel percorso specificato in dir
             
-                boolean created = new File(dir).mkdirs();
-                return created;
-                
-        }
+        boolean created = new File(dir).mkdirs();
+        return created;        
+    }
 
-        public static boolean exist(String file){  //metodo per controllare l'esistenza di un file;ritorna vero se esiste;
+    public static boolean exist(String file){
 
-            File check = new File(file);
-
-            boolean value = check.exists();
-
-
-            return value;
-
-        }
-
-        public String getOS(){       //metodo che mi dice se il sistema ospitante è Microsoft o altro;
-
-            Properties x = System.getProperties();
-            boolean value = x.contains("Windows");
-            if(value){
-                    return "Windows";
-            }
-            else{
-                return "others";
-            }
-
-        }
-       
-      
-     public static void writeFile(int idUser,InputStream reader) throws IOException{
+        File check = new File(file);
+        boolean value = check.exists();
+        
+        return value;
+    }
+    
+    /*Scrittura di un file fisico*/
+    public void writeFile(int idUser,InputStream reader) throws IOException{
         
         int flag;
         OutputStream writer;
@@ -64,58 +40,52 @@ public class fileOperation {
         writer = new FileOutputStream(im);
         buf = new byte[1024];
         
-        while ((flag = reader.read(buf)) > 0) {
-        writer.write(buf, 0, flag);
+        while ((flag = reader.read(buf)) > 0){
+            writer.write(buf, 0, flag);
         }
         
         reader.close();
-        writer.close();
-               
+        writer.close();               
     }   
         
-        
-     public static ImageIcon loadImage(File selectedFile) throws IOException{
+    /*Ritorna un oggetto ImageIcon dato un file; l'oggetto ritornato può essere utilizzato
+     * per utilizzare new JLabel.setIcon(imagicon);
+     */
+    public ImageIcon loadImage(File selectedFile) throws IOException{
 
-    
         Image image = ImageIO.read(selectedFile);
         Image newimg = image.getScaledInstance(182, 200, java.awt.Image.SCALE_SMOOTH);
-        ImageIcon icon = new ImageIcon(newimg);
-    
+        ImageIcon icon = new ImageIcon(newimg);    
         return icon;
+     }
+     
+    /*Permette al server di eliminare eventuali File e Cartelle*/    
+    public boolean deleteFiles(File toDelete){  //metodo per cancellare cartelle;
     
-}  
-        
-        
-
-
-        public boolean deleteDirectory(File toDelete){  //metodo per cancellare cartelle;
-
-
-               if(toDelete.isDirectory()){
-                        String[] content = toDelete.list();
-                        for(int index=0;index < content.length;index++){
-                            deleteDirectory(new File(toDelete,content[index]));
-                        }                        
+        if(toDelete.isDirectory()){
+            String[] content = toDelete.list();
+            for(int index=0;index < content.length;index++)
+                deleteFiles(new File(toDelete,content[index]));
         }
         return toDelete.delete();
     }
 
-        
-        public static long fileSize(String path){
+    /*Attualmente non usato ma possibile uso in futuri aggiornamenti dell'applicazione soprattutto nel
+     * caso di inserimenti di "Limitazioni" nell'uso di alcune risorse da parte dell'utente;
+     */
+    public long fileSize(String path){
 	
-                File f = new File(path);
-                long file_size = 0;
-
-		if (f.exists()) {
-			file_size = f.length();
-			return file_size;
-                }
-
-                else{
-                    return 0;
-                }
-
-        }
+        File f;
+        long file_size;
+        file_size = 0;
+        f = new File(path);
+            if(f.exists()){
+                file_size = f.length();
+                return file_size;
+            }else{
+                return 0;
+            }
+     }
 
 }
 
